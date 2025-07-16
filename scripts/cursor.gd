9118da5416c543f09cc45993866afc1b74e20e43
nytouu <capitaine7312@gmail.com>
2025-07-16 16:38:11 +0200

    add highlightable paintings

--- scripts/cursor.gd
+++ scripts/cursor.gd
@@ -2,10 +2,12 @@ extends Node3D
 
 @onready var camera: Camera3D = %Camera
 @onready var mesh: Node3D = $CursorArrow
+@onready var ray_cast: RayCast3D = $CursorArrow/RayCast3D
 
 @export var ray_length: float = 2000.0
 
 var _last_position: Vector3
+var _last_selected_frame: Frame
 
 enum RAY_RESULT { POSITION, ROTATION }
 
@@ -16,7 +18,7 @@ func _screen_to_point() -> Array:
 	var ray_origin = camera.project_ray_origin(mouse_pos)
 	var ray_end = ray_origin + camera.project_ray_normal(mouse_pos) * ray_length
 
-	var intersected = space_state.intersect_ray(PhysicsRayQueryParameters3D.create(ray_origin, ray_end))
+	var intersected = space_state.intersect_ray(PhysicsRayQueryParameters3D.create(ray_origin, ray_end))	
 	if intersected.has("position"):
 		return [intersected["position"], intersected["normal"]]
 	return [Vector3(), Vector3()]
@@ -42,3 +44,17 @@ func _process(_delta: float) -> void:
 			mesh.rotation = Vector3(-PI / 2, 0, 0)
 		Vector3(1, 0, 0):
 			mesh.rotation = Vector3(0, 0, -PI / 2)
+			
+func _physics_process(_delta: float) -> void:
+	var obj: StaticBody3D
+	
+	if ray_cast.is_colliding():
+		obj = ray_cast.get_collider()
+		if obj is StaticBody3D:
+			_last_selected_frame = obj.get_parent() as Frame
+			_last_selected_frame.set_highlight(true)
+	else:
+		if _last_selected_frame: _last_selected_frame.set_highlight(false)
+
+	if Input.is_action_just_pressed("frame_select") && obj:
+		pass
